69ee3dfe142b7de2e56b43d25a41487f3959e9e0
denise813 <denise813@163.com>
2020-12-01 22:56:57 -0500

    modify mirror sorce script

--- depoly_rpm_service.sh
+++ depoly_rpm_service.sh
@@ -1,11 +1,30 @@
-yum install httpd createrepo -y
+rm -rf /var/www/html/ceph
+rm -rf /var/www/html/repo/centos8/ceph.repo
+rm -rf /etc/yum.repos.d/ceph.repo
+
+yum install httpd createrepo wget -y
 mkdir -p /var/www/html/ceph
+mkdir -p /var/www/html/repo/centos8
+
+cp /root/package/* /var/www/html/ceph/
+
 cd /var/www/html/ceph/
-
-#cp /root/cephadm-15.2.5-0.el8.x86_64.rpm ./
 createrepo -v /var/www/html/ceph/
-systemctl start httpd.service
-cp ./ceph.repo /etc/yum.repos.d/
-yum list
 
-#yum install cephadm
+systemctl start httpd.service
+systemctl stop firewalld.service
+mirror_node=$(/sbin/ifconfig eno1 | awk '/inet/ {print $2}' | cut -f2 -d ":" |awk 'NR==1 {print $1}')
+
+cat >> /var/www/html/repo/centos8/ceph.repo <<EOF
+[ceph]
+name=ceph
+baseurl=http://${mirror_node}/ceph
+enabled=1
+gpgcheck=0
+priority=1
+EOF
+
+wget -P /etc/yum.repos.d/ http://${mirror_node}/repo/centos8/ceph.repo
+yum clean all
+yum makecache
+
