1f69a77c06fd400974a6ee7b823ba0d4badee758
denise813 <denise813@163.com>
2020-12-09 09:16:51 -0500

    add

--- prepare_osd.sh
+++ prepare_osd.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#set -x
+set -x
 
 while [ $# -gt 0 ];
 do
@@ -22,53 +22,59 @@ do
 	-ds) db_disk_size=$2
 		shift
 		;;
+	-sid) start_osd_id=$2
    esac
    shift
 done
 
-blocks=${data_disks//,/ }
+bdisks=${data_disks//,/ }
 echo "cache=${cache_disk} meta=${meta_disk} wal_size=${wal_disk_size} db_size=${db_disk_size} block=${blocks}"
 
 # cache 不为空
-if [ ! -n "${cache_disk}" ]
+if [ ${cache_disk} ]
 then
 	sdparm -s WCE=0 /dev/${cache_disk}
+	dd if=/dev/zero of=/dev/${cache_disk} bs=1M count=10
 	parted /dev/${cache_disk} --script mktable gpt
 fi
 
 # meta 不为空
-if [ ! -n "${meta_disk}" ]
+if [ ${meta_disk} ]
 then
 	sdparm -s WCE=0 /dev/${meta_disk}
+	dd if=/dev/zero of=/dev/${meta_disk} bs=1M count=10
 	parted /dev/${meta_disk} --script mktable gpt
 fi
 
 # block 处理
-for block in blocks
+for disk in ${bdisks[*]}
 do
-	sdparm -s WCE=0 /dev/${block}
-	parted /dev/${block} --script mktable gpt
+	sdparm -s WCE=0 /dev/${disk}
+	dd if=/dev/zero of=/dev/${disk} bs=1M count=10
 done
 
+tmp_link_dir=/tmp/osd/link
+mkdir -p ${tmp_link_dir}
+rm -rf /${tmp_link_dir}/*
 function split_cache_disk
 {
 	start=0
 	end=0
 	i=0
-	cache_disk=$1
+	cdisk=$1
 	disk_size=$2
-	for block in ${blocks[*]}
+	for disk in ${bdisks[*]}
 	do
 		let "i+=1"
 		let "start=end"
 		let "end=start + cache_size"
-		parted /dev/${cache_disk} --script mkpart primary ${start}G ${end}G
-		ln -s /dev/${cache_disk}p${i} dev/cache_${block}
+		parted /dev/${cdisk} --script mkpart primary ${start}G ${end}G
+		ln -s /dev/${cdisk}p${i} /${tmp_link_dir}/cache_${disk}
 	done
 }
 
 # 将缓存盘分成多个区
-if [ ! -n "${cache}" ]
+if [ ${cache_disk} ]
 then
 	split_cache_disk ${cache_disk} ${cache_size}
 fi
@@ -79,69 +85,92 @@ function split_meta_disk
 	end=0
 	start=0
 	i=0
-	meta_disk=$1
+	mdisk=$1
 	wal_size=$2
 	db_size=$3
-	for block in ${blocks[*]}
+	for disk in ${bdisks[*]}
 	do
 		let "i+=1"
 		let "start=end"
 		let "end=start + wal_size"
-		parted /dev/${meta_disk} --script mkpart primary ${start}G ${end}G
-		ln -s /dev/${meta_disk}p${i} /dev/wal_${block}
+		parted /dev/${mdisk} --script mkpart primary ${start}G ${end}G
+		ln -s /dev/${mdisk}p${i} /${tmp_link_dir}/wal_${disk}
 		let "i+=1"
 		let "start=end"
 		let "end=start + db_size"
-		parted /dev/${meta_disk} --script mkpart primary ${start}G ${end}G
-		ln -s /dev/${meta_disk}p${i} /dev/db_${block}
+		parted /dev/${mdisk} --script mkpart primary ${start}G ${end}G
+		ln -s /dev/${mdisk}p${i} /${tmp_link_dir}/db_${disk}
 	done
 }
 
 # 将元数据划分为多个分区
-if [ ! -n "${meta}" ]
+if [ ${meta_disk} ]
 then
 	split_meta_disk ${meta_disk} ${wal_disk_size} ${db_disk_size}
 fi
 
+function clean_osd
+{
+	osd_curr_id=$1
+	$(ceph osd down osd.${osd_curr_id})
+	$(ceph osd out osd.${osd_curr_id})
+        $(ceph osd crush remove osd.${osd_curr_id})
+	$(ceph auth del osd.${osd_curr_id})
+	$(ceph osd rm osd.${osd_curr_id})
+	rm -rf /var/lib/ceph/osd/ceph-${osd_curr_id}
+}
+
 # 获取当前空闲osd
-for block in ${blocks[*]}
+start_id=0
+osd_id=${start_id}
+for disk in ${bdisks[*]}
 do
 	cache_param=""
 	wal_param=""
 	db_param=""
-	if [ -L /dev/cache_${block} ]; then
-		cache_param="--block.cache /dev/cache_${block}"
+	if [ -L /${tmp_link_dir}/cache_${disk} ]; then
+		divice=$(readlink /${tmp_link_dir}/cache_${disk})
+		cache_param="--block.cache ${divice}"
 	fi
 
-	if [ -L /dev/wal_${block} ]; then
-		wal_param="--block.wal /dev/wal_${block}"
+	if [ -L /${tmp_link_dir}/wal_${disk} ]; then
+		divice=$(readlink /${tmp_link_dir}/wal_${disk})
+		wal_param="--block.wal ${divice}"
 	fi
 
-	if [ -L /dev/db_${block} ]; then
-		db_param="--block.db /dev/db_${block}"
+	if [ -L /${tmp_link_dir}/db_${disk} ]; then
+		divice=$(readlink /${tmp_link_dir}/db_${disk})
+		db_param="--block.db ${divice}"
 	fi
 
-    	/bin/bash -c ulimit -n 32768;ceph-volume raw prepare --bluestore --data ${block} ${wal_param} ${db_param} ${cache_param}  --no-tmpfs
+	clean_osd ${osd_id}
+    	/bin/bash -c ulimit -n 32768;ceph-volume raw prepare --bluestore --data /dev/${disk} ${wal_param} ${db_param} ${cache_param} --osd_id ${osd_id} --no-tmpfs
+	let "osd_id += 1"
 done
 
 # 激活 osd磁盘
-for block in ${blocks[*]}
+for disk in ${bdisks[*]}
 do
 	cache_param=""
 	wal_param=""
 	db_param=""
-	if [ -L /dev/cache_${block} ]; then
-		cache_param="--block.cache /dev/cache_${block}"
+	if [ -L /${tmp_link_dir}/cache_${disk} ]; then
+		divice=$(readlink /${tmp_link_dir}/cache_${disk})
+		cache_param="--block.cache ${divice}"
 	fi
 
-	if [ -L /dev/wal_${block} ]; then
-		wal_param="--block.wal /dev/wal_${block}"
+	if [ -L /${tmp_link_dir}/wal_${disk} ]; then
+		divice=$(readlink /${tmp_link_dir}/wal_${disk})
+		wal_param="--block.wal ${divice}"
 	fi      
 
-	if [ -L /dev/db_${block} ]; then
-		db_param="--block.db /dev/db_${block}"
+	if [ -L /${tmp_link_dir}/db_${disk} ]; then
+		divice=$(readlink /${tmp_link_dir}/db_${disk})
+		db_param="--block.db ${divice}"
 	fi
 
 	# ceph-volume lvm activate -h
-	/bin/bash -c ulimit -n 32768;ceph-volume raw activate --device ${block} ${wal_param} ${db_param} ${cache_param} --no-tmpfs --no-systemd
+	/bin/bash -c ulimit -n 32768;ceph-volume raw activate --device /dev/${disk} ${wal_param} ${db_param} ${cache_param} --no-tmpfs --no-systemd
 done
+
+rm -rf /${tmp_link_dir}/*
